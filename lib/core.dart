d3e887b10d4f51f55d563e3ca8fd95fe93476358
Asepimam <17200931@bsi.ac.id>
2023-01-28 09:49:53 +0700

    First Commit Set Up

---
+++ lib/core.dart
@@ -0,0 +1,18 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:fhe_template/module/Auth/login/controller/login_controller.dart';
+export 'package:fhe_template/module/Auth/login/view/login_view.dart';
+export 'package:fhe_template/module/Auth/logout/controller/logout_controller.dart';
+export 'package:fhe_template/module/Auth/logout/view/logout_view.dart';
+export 'package:fhe_template/module/Auth/register/controller/register_controller.dart';
+export 'package:fhe_template/module/Auth/register/view/register_view.dart';
+export 'package:fhe_template/module/Produk/book_edit_add/controller/book_edit_add_controller.dart';
+export 'package:fhe_template/module/Produk/book_edit_add/view/book_edit_add_view.dart';
+export 'package:fhe_template/module/Produk/books/controller/books_controller.dart';
+export 'package:fhe_template/module/Produk/books/view/books_view.dart';
+export 'package:fhe_template/module/Produk/books_detail/controller/books_detail_controller.dart';
+export 'package:fhe_template/module/Produk/books_detail/view/books_detail_view.dart';
+export 'setup.dart';
